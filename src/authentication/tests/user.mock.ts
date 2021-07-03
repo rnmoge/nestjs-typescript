a8065b40889d1de50b4b7b513ff8d5658eecbb7d
Reinaldo Moge <rnmoge@yahoo.com.br>
2021-07-03 09:35:16 -0300

    Initial commit

---
+++ src/authentication/tests/user.mock.ts
@@ -0,0 +1,18 @@
+import User from '../../users/user.entity';
+
+const mockedUser: User = {
+  id: 1,
+  email: 'user@email.com',
+  name: 'John',
+  password: 'hash',
+  stripeCustomerId: 'stripe_customer_id',
+  address: {
+    id: 1,
+    street: 'streetName',
+    city: 'cityName',
+    country: 'countryName'
+  },
+  isTwoFactorAuthenticationEnabled: false
+}
+
+export default mockedUser;
---
+++ src/credit-cards/creditCards.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Post, Req, UseGuards, Get, HttpCode } from '@nestjs/common';
+import JwtAuthenticationGuard from '../authentication/jwt-authentication.guard';
+import RequestWithUser from '../authentication/requestWithUser.interface';
+import StripeService from '../stripe/stripe.service';
+import AddCreditCardDto from './dto/addCreditCardDto';
+import SetDefaultCreditCardDto from './dto/setDefaultCreditCard.dto';
+
+@Controller('credit-cards')
+export default class CreditCardsController {
+  constructor(
+    private readonly stripeService: StripeService
+  ) {}
+
+  @Post()
+  @UseGuards(JwtAuthenticationGuard)
+  async addCreditCard(@Body() creditCard: AddCreditCardDto, @Req() request: RequestWithUser) {
+    return this.stripeService.attachCreditCard(creditCard.paymentMethodId, request.user.stripeCustomerId);
+  }
+
+  @Post('default')
+  @HttpCode(200)
+  @UseGuards(JwtAuthenticationGuard)
+  async setDefaultCard(@Body() creditCard: SetDefaultCreditCardDto, @Req() request: RequestWithUser) {
+    await this.stripeService.setDefaultCreditCard(creditCard.paymentMethodId, request.user.stripeCustomerId);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthenticationGuard)
+  async getCreditCards(@Req() request: RequestWithUser) {
+    return this.stripeService.listCreditCards(request.user.stripeCustomerId);
+  }
+}
